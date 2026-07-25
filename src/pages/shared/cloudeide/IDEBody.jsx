b28bdd5670b945fa26f60961bf8705b949e546b6
Yasir Gill <yasirgill3637@gmail.com>
2026-07-25 15:43:44 +0500

    preview code updated

--- src/pages/shared/cloudeide/IDEBody.jsx
+++ src/pages/shared/cloudeide/IDEBody.jsx
@@ -1306,7 +1306,7 @@ const IDEBody = () => {
 
           const previewUrl = isLocal
             ? `http://localhost:${projectPort}/`
-             : `https://preview.ucollyx.com/api/proxy/${slug}/`;
+             : `https://preview.ucollyx.com/preview/${slug}/`;
             // : `${window.location.protocol}//${window.location.host}/preview/${slug}/`;
 
           return (
