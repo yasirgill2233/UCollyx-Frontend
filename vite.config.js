bbcc725a7518f0c042ce770cd07886f31e3afd19
Yasir Gill <yasirgill3637@gmail.com>
2026-07-25 14:03:26 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {  
-    // Ya agar aap har kisi host ko allow karna chahte hain Sandbox environment main:
-    allowedHosts: true
-  }
 })
