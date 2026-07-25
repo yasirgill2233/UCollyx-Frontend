9f19af50d0cf397841a689e7668c8b9148c0e743
Yasir Gill <yasirgill3637@gmail.com>
2026-07-25 13:52:10 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 5180,
-    // Dynamic preview host allow kar do
-    allowedHosts: [
-      'preview.ucollyx.com',
-      '.ucollyx.com', // Wildcard subdomains allow karne ke liye
-    ],
+  server: {  
     // Ya agar aap har kisi host ko allow karna chahte hain Sandbox environment main:
-    // allowedHosts: true
+    allowedHosts: true
   }
 })
