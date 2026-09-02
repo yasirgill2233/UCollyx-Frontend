d9a018f8048a85c4479edc6296440f260df32dce
Yasir Saleem <yasirgill3637@gmail.com>
2026-09-02 18:43:32 +0500

    updated login updated on mobile screen

--- src/components/layout/LandingPage.jsx
+++ src/components/layout/LandingPage.jsx
@@ -117,19 +117,9 @@ function LandingPage() {
 
           {/* Mobile button */}
           <button className="rounded-lg p-2 text-slate-700 md:hidden">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+            <button onClick={() => navigate('/login')} className="rounded-xl bg-blue-600 px-5 py-2.5 text-sm font-semibold text-white shadow-lg shadow-blue-600/20 transition hover:bg-blue-700 hover:cursor-pointer">
+              Log in
+            </button>
           </button>
         </div>
       </header>
